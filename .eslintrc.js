8a4634f68fc54de7883b1085392174ed0359950e
1freelipe <feliperodrigues_spfc@hotmail.com>
2025-09-23 14:30:08 -0300

    separando os componentes usuais

--- .eslintrc.js
+++ .eslintrc.js
@@ -31,6 +31,7 @@ module.exports = {
     'react/jsx-filename-extension': 0,
     'react/state-in-constructor': 0,
     'import/no-extraneous-dependencies': 0,
+    'react/forbid-prop-types': 0,
   },
   settings: {
     react: {
--- src/components/main.js
+++ src/components/main.js
@@ -1,11 +1,8 @@
 import React, { Component } from 'react';
 import './main.css';
 
-// eslint-disable-next-line
-import { FaPlus } from 'react-icons/fa';
-
-// eslint-disable-next-line
-import { FaEdit, FaWindowClose } from 'react-icons/fa';
+import Form from './Form';
+import Tarefas from './Tarefas';
 
 export default class Main extends Component {
   state = {
@@ -77,6 +74,7 @@ export default class Main extends Component {
       this.setState({
         tarefas: [...novasTarefas],
         index: -1,
+        novaTarefa: '',
       });
     }
   };
@@ -88,25 +86,18 @@ export default class Main extends Component {
     return (
       <div className="main">
         <h1>Lista de Tarefas</h1>
-        <form onSubmit={this.handleSubmit} action="#" className="form">
-          <input onChange={this.handleChange} type="text" value={novaTarefa} />
-          <button type="submit">
-            <span><FaPlus /></span>
-          </button>
-        </form>
 
-        <ul className="tarefas">
-          {tarefas.map((tarefa, index) => (
-            <li key={tarefa}>
-              {tarefa}
-              <span>
-                <FaEdit onClick={(e) => this.handleEdit(e, index)} className="edit" />
-                <FaWindowClose onClick={(e) => this.handleDelete(e, index)} className="delete" />
-              </span>
-            </li>
-          ))}
+        <Form
+          handleSubmit={this.handleSubmit}
+          handleChange={this.handleChange}
+          novaTarefa={novaTarefa}
+        />
 
-        </ul>
+        <Tarefas
+          tarefas={tarefas}
+          handleDelete={this.handleDelete}
+          handleEdit={this.handleEdit}
+        />
 
       </div>
     );
